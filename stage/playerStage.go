c4225572b2a22929d42109973c4f732ef9f669be
Nick Davies <git@nicolasdavies.com.au>
2013-12-30 23:03:48 +1100

    Fixed small bugs in stage code

--- stage/playerStage.go
+++ stage/playerStage.go
@@ -14,7 +14,7 @@ func (ps *PlayerStage) InitMap() {
         }
 
         for row := 0; row < Wall_Bottom; row++ {
-            ps.Tiles[col][PlayerStageHeight - row] = T_Wall
+            ps.Tiles[col][PlayerStageHeight - row - 1] = T_Wall
         }
     }
 
@@ -22,18 +22,18 @@ func (ps *PlayerStage) InitMap() {
     for col := 0; col < Wall_Side; col++ {
         for row := 0; row < PlayerStageHeight; row++ {
             ps.Tiles[col][row] = T_Wall
-            ps.Tiles[PlayerStageWidth - col][row] = T_Wall
+            ps.Tiles[PlayerStageWidth - col - 1][row] = T_Wall
         }
     }
 
     // Setup goal and spawn area
     for col := Wall_Side; col < (PlayerStageWidth - Wall_Side); col++ {
         for row := 0; row < Spawn_Size; row++ {
-            ps.Tiles[col][row] = T_Spawn
+            ps.Tiles[col][Wall_Top + row] = T_Spawn
         }
 
         for row := 0; row < Wall_Bottom; row++ {
-            ps.Tiles[col][PlayerStageHeight - row] = T_Goal
+            ps.Tiles[col][PlayerStageHeight - row - Wall_Bottom - 1] = T_Goal
         }
     }
 
